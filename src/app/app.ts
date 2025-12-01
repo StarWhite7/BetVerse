769639264e32d14bb26fd9298436816d79a4c466
StarWhite <xxstarwhitexx57@gmail.com>
2025-12-01 11:56:49 +0100

    Neon style comming soon

--- src/app/app.ts
+++ src/app/app.ts
@@ -9,4 +9,21 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('BetVerse');
+
+  progress = 0;
+
+  constructor() {
+    this.startLoading();
+  }
+
+  startLoading() {
+    const interval = setInterval(() => {
+      if (this.progress < 100) {
+        this.progress++;
+      } else {
+        clearInterval(interval);
+      }
+    }, 25920000); // durée du remplissage (40ms = ~4s total)
+  }
 }
+
